3aa80dd002fdc9313178add9790fdd686c73bbb1
ofa <oafridah@gmail.com>
2025-12-24 00:37:54 +0300

    Refactor print statements to use formatted strings
    Updated print formatting to use formatted strings and improved readability.

--- LearningtheJavaLanguage/NumbersAndStrings/Strings/PrintfAndFormat.java
+++ LearningtheJavaLanguage/NumbersAndStrings/Strings/PrintfAndFormat.java
@@ -4,16 +4,28 @@ public class PrintfAndFormat {
         int intVar = 2;
         String stringVar = "two";
         // instead of a one-time print statement like this:
-       System.out.printf("The value of the float " +
+       String fs1 = System.out.printf("The value of the float " +
                         "variable is %f, while " +
                         "the value of the " +
                         "integer variable is %d, " +
                         "and the string is %s",
-                floatVar, intVar, stringVar);
+                floatVar, intVar, stringVar).toString();
+
+        System.out.println("\n" + fs1);
+
+       String fs2 = """
+                        The value of the float
+                        variable is %f, while 
+                        the value of the 
+                        integer variable is %d
+                        and the string is %s
+                        """.formatted(floatVar, intVar, stringVar);
+
+        System.out.println("\n" + fs2);
 
         // ... create a formatted string that you can reuse like this:
         String fs;
-        fs = String.format("The value of the float " +
+        fs = String.format("\nThe value of the float " +
                         "variable is %f, while " +
                         "the value of the " +
                         "integer variable is %d, " +
